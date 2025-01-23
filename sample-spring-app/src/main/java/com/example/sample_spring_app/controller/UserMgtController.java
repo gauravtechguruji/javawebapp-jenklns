9ea0b5e41a49e3b4ae3449717eb2ec1ec32d8828
shikhilsaxena <shikhilsaxena@shikhilsaxena.cdacnoida.in>
2025-01-23 18:16:41 +0530

    New changes make

--- sample-spring-app/src/main/java/com/example/sample_spring_app/controller/UserMgtController.java
+++ sample-spring-app/src/main/java/com/example/sample_spring_app/controller/UserMgtController.java
@@ -1,22 +1,34 @@
 package com.example.sample_spring_app.controller;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.sample_spring_app.model.User;
+import com.example.sample_spring_app.service.UserManagementService;
 
 @RestController
 public class UserMgtController {
 
-    @RequestMapping("/users")
-    public User[] getUserList() {
+    @Autowired
+    UserManagementService userManagementService;
 
-        List<User> userList = new ArrayList();
-        userList.add(new User("Joy", "1234-scdw2-2fcevw342-2323"));
-        userList.add(new User("Chandler", "3454-2csfdfd-sfdvsdjv-7623"));
-        return userList.toArray(new User[0]);
+    @GetMapping("/users")
+    public User[] getUserList() {
+        return userManagementService.getUserList().toArray(new User[0]);
+    }
+
+    @PostMapping("/users/add")
+    public void addUser(@RequestBody User user) {
+        userManagementService.addUserList(user);
+    }
+    @GetMapping("/users/{Username}")
+    public String getUserId(@PathVariable("Username") String username){
+        return userManagementService.getUerId(username);
     }
 }
